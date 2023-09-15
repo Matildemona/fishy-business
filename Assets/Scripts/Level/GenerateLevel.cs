0f1882492dcaf3954fbd5ed3804a263d2adbc91a
Matildemona <mati6934@stud.kea.dk>
2023-09-15 18:59:51 +0200

    Level Generator Fix

--- Assets/Scripts/Level/GenerateLevel.cs
+++ Assets/Scripts/Level/GenerateLevel.cs
@@ -10,29 +10,25 @@ public class GenerateLevel : MonoBehaviour
     public int chosenSecNum;
     public float waitBeforeDestroy = 5f;
 
-    private GameObject previousSection; // Store a reference to the previous section
+    private GameObject previousSection;
+    [SerializeField] int timeBeforeLevelDestroy = 16;
+    [SerializeField] GameObject startLevel;
 
 
     public void GenerateSection()
     {
         chosenSecNum = Random.Range(0, 2);
 
-        // Destroy the previous section if it exists
-        if (previousSection != null)
-        {
-            //This does not work.
-            DestroySection();
-        }
-
         previousSection = Instantiate(section[chosenSecNum], new Vector3(0, 0, zPos), Quaternion.identity);
+
+        Destroy(previousSection, timeBeforeLevelDestroy);
+        
+        if (startLevel != null)
+        {
+            Destroy(startLevel, timeBeforeLevelDestroy);
+        }
         
         zPos += 100f;
         creatingSection = false;
-    }
-
-    IEnumerator DestroySection()
-    {
-        yield return new WaitForSeconds(waitBeforeDestroy);
-        Destroy(previousSection);
     }
 }
