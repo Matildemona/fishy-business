e2282dfa3436a3d2725f7aaef541cf4fc94f703a
Matildemona <mati6934@stud.kea.dk>
2023-09-15 17:52:39 +0200

    Prefabs + Fixes
    added the player model, did the eel prefab and fixed the spear bug.

---
+++ Assets/Scripts/Enemies & Animations/EelAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EelAnimation : MonoBehaviour
+{
+    [SerializeField] float movingDistance = 10f;
+    [SerializeField] float movingDuration = 2f;
+
+    void Start()
+    {
+        transform.DOMoveX(movingDistance, movingDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuint);
+    }
+
+    public int damageValue = 20;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<PlayerHealth>().TakeDamage(damageValue);
+        }
+    }
+}
